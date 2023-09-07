6b272989443606daaa00837e0731a0f82694b9fe
Arturo <codealgr@gmail.com>
2023-09-07 21:00:34 +0200

    Create sum function

--- Test/src/hello_world/Hello.java
+++ Test/src/hello_world/Hello.java
@@ -6,5 +6,15 @@ public class Hello {
 		System.out.println("Hello World!!");
 		System.out.println("<------------------>");
 		System.out.println("Esto es una suma");
+		
+		int num1 = 5;
+		int num2 = 10;
+
+		int result = sum(num1, num2);
+		System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+	}
+	
+	public static int sum(int a, int b) {
+		return a + b;
 	}
 }
