e811e4039fa1ec69f9f1e65b86b3d4a35ef41412
Arturo <codealgr@gmail.com>
2023-09-09 01:04:20 +0200

    Create new Class Persona and an object for this class

---
+++ Test/src/persona/Main.java
@@ -0,0 +1,12 @@
+package persona;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Persona persona1 = new Persona(19, "Arturo", "Largo García", "54957299h", 1.75);
+		
+		System.out.println(persona1.getNombre());
+		
+	}
+}
