ee19a876fdff166c310f12b625d1dbb9be5bbd57
Arturo <codealgr@gmail.com>
2023-11-20 12:51:21 +0100

    yo q se

--- Test/exam/edu/ucam/exam/Server.java
+++ Test/exam/edu/ucam/exam/Server.java
@@ -6,40 +6,40 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Server extends Connection{
-	
+public class Server extends Connection {
+
 	private List<Socket> clientList = new ArrayList<>();
-	
-	public Server() throws IOException{
+
+	public Server() throws IOException {
 		super("Servidor");
 	}
-	
+
 	public void addClient(Socket sc) {
 		this.clientList.add(sc);
 	}
-	
+
 	public void showClientList(PrintWriter printWriter) {
 		for (Socket socket : clientList) {
 			printWriter.println(socket);
 			printWriter.flush();
 		}
 	}
-	
-	public void start() throws IOException{
-		
+
+	public void start() throws IOException {
+
 		System.out.println("SERVIDOR INICIADO EN EL PUERTO : " + getPort());
 		System.out.println("========================================================\n");
-		
-		while(true) {
+
+		while (true) {
 			Socket clientSocket = ss.accept();
 			System.out.println("Cliente Aceptado en el servidor : " + clientSocket.getRemoteSocketAddress());
-			
+
 			this.addClient(clientSocket);
 			serverOut = inicializarPrintWriter(clientSocket);
-			
+
 			ServerThread serverThread = new ServerThread(clientSocket, serverOut, this);
 			serverThread.start();
-			
+
 		}
 	}
 }
